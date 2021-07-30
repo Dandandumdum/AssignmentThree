6a9ecb00ba299520a20e01ab6e32a960a4422fc2
Daniel Dumville <danieldumville@gmail.com>
2021-07-30 08:47:52 +0200

    Added data.sql with seeded values

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO franchise (name, description) VALUES ('MCU', 'Lots of heroes running around.');
+INSERT INTO franchise (name, description) VALUES ('CHEF TRIALS', 'Tough as nails chefs from across time come together.');
+INSERT INTO franchise (name, description) VALUES ('Monster Mash', 'Every monster you can think off hanging out.');
+
+INSERT INTO character (alias, full_name, gender, picture) VALUES ('The Invisible Man','Dave Unseen','Cannot Tell', 'https://www.royalunibrew.com/wp-content/uploads/2021/07/blank-profile-picture-973460_640.png');
+INSERT INTO character (alias, full_name, gender, picture) VALUES ('Revenge Chef', 'Gordon Revenge', 'Male', 'https://i.dailymail.co.uk/1s/2021/06/21/09/44480025-9708011-image-a-1_1624263083294.jpg');
+
+INSERT INTO movie (director, genre, movie_title, picture, release_year, trailer) VALUES ('Peter Jackson', 'Adventure', 'King Kong', '2005', 'https://www.youtube.com/watch?v=AYaTCPbYGdk&ab_channel=MovieclipsClassicTrailers');
+INSERT INTO movie (director, genre, movie_title, picture, release_year, trailer) VALUES ('Brad Bird', 'Cooking Action', 'Ratatouille', '2007', 'https://www.imdb.com/video/vi906147865?playlistId=tt0382932&ref_=tt_ov_vi');
+INSERT INTO movie (director, genre, movie_title, picture, release_year, trailer) VALUES ('Jon Favreau', 'Action', 'Iron Man', '2008', 'https://www.imdb.com/video/vi447873305?playlistId=tt0371746&ref_=tt_ov_vi');
